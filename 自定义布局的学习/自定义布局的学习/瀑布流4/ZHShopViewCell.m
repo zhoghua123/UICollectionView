8c6bf8ca963bf8f927576d20bdb80985c4f7bb6d
Your Name <you@example.com>
2017-11-08 14:27:27 +0800

    f

--- 自定义布局的学习/自定义布局的学习/瀑布流4/ZHShopViewCell.m
+++ 自定义布局的学习/自定义布局的学习/瀑布流4/ZHShopViewCell.m
@@ -17,10 +17,6 @@
 @implementation ZHShopViewCell
 
 - (void)setShop:(ZHShop *)shop{
-    
-    
-    
-    
     _shop = shop;
     // 1.图片
     [self.imageView sd_setImageWithURL:[NSURL URLWithString:shop.img] placeholderImage:[UIImage imageNamed:@"loading"]];
